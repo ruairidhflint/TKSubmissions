bb24431b4f058ca8b94d1d6ce151a4d7f8f29e14
Rory Flint <roryflintphoto@gmail.com>
2020-02-16 09:50:06 +0000

    Add binary_search working implementation

--- Week2/Day1.py
+++ Week2/Day1.py
@@ -20,42 +20,30 @@ def linear_search(arr, x):
 
 
 def binary_search(sortedArr, x):
-    high = len(sortedArr) - 1
     low = 0
-    middle = (len(sortedArr) - 1) // 2
-
-    while sortedArr[middle] != x:
-        if sortedArr[middle] == x:
-            print(high)
-            print(low)
-            print(middle)
-            return sortedArr[middle]
-
-        elif sortedArr[middle] > x:
-            high = middle
-            middle = middle // 2
-
-        elif sortedArr[middle] < x:
-            high = len()
-            middle = middle // 2
-            print(high)
-            print(low)
-            print(middle)
-            return "Middle value is smaller"
-
-    return sortedArr[middle] - 1
+    high = len(sortedArr) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if sortedArr[mid] > x:
+            high = mid - 1
+        elif sortedArr[mid] < x:
+            low = mid + 1
+        else:
+            return mid
+    return "Does not exist in list"
 
 
 test_arr = [1, 2, 3, 4, 5, 6, 7]
 test_arr2 = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
-print(binary_search([1, 2, 3, 4, 5, 6, 7], 3))
+print(binary_search([1, 2, 3, 4, 5, 6, 7], 7))
 
 
 '''
 
-What will the contents of the array below look like after each pass of the Selection Sort algorithm? 
+What will the contents of the array below look like after each pass of the Selection Sort algorithm?
 
 [25, 67, 4, 33, 19, 40]
 [4, 67, 25, 33, 19, 40]
@@ -63,7 +51,7 @@ What will the contents of the array below look like after each pass of the Selec
 [4, 19, 25, 33, 67, 40]
 [4, 19, 25, 33, 67, 40]
 
-What will the contents of the same array look like after each pass of the Insertion Sort algorithm? 
+What will the contents of the same array look like after each pass of the Insertion Sort algorithm?
 [25, 67, 4, 33, 19, 40]
 [4, 25, 67, 33, 19, 40]
 [4, 25, 33, 67, 19, 40]
