3badbb3806778f70ad69cc7ce8271a8919e4c5dc
Rory Flint <roryflintphoto@gmail.com>
2020-02-17 22:11:45 +0000

    Add merge sort pseudo

--- Week2/Day2.py
+++ Week2/Day2.py
@@ -1,31 +1,58 @@
 '''Write a recursive search function that receives as input an array of integers and a target integer value. This function should return True if the target element exists in the array, and False otherwise.
 '''
 
+
 def recursive_search(arr, target):
     if len(arr) == 0:
         return False
-    
+
     if arr[0] == target:
         return True
     else:
         return recursive_search(arr[1:], target)
 
 
-print(recursive_search([1,2,3,4,5,6,7,8], 5))
+print(recursive_search([1, 2, 3, 4, 5, 6, 7, 8], 5))
 
-def recursive_search_two(arr,target,position=0):
+
+def recursive_search_two(arr, target, position=0):
     if position == len(arr) - 1:
         return False
 
     if arr[position] == target:
         return True
-    else: 
+    else:
         return recursive_search_two(arr, target, position + 1)
 
-print(recursive_search_two([1,2,3,4,5,6,7,8], 50))
 
-'''What would be the base case(s) we’d have to consider for implementing this function?
+print(recursive_search_two([1, 2, 3, 4, 5, 6, 7, 8], 50))
 
-- For first implementation: If the list has length or if the list target and the list position match
-- For second implementation: if the position is equal to the length of the list
+'''
+    What would be the base case(s) we’d have to consider for implementing this function?
+
+    - For first implementation: If the list has length or if the list target and the list position match
+    - For second implementation: if the position is equal to the length of the list
+
+'''
+
+'''
+    How should our recursive solution converge on our base case(s)?
+
+    -Implementation 1: The length of the array checked descreases each time
+    - by increasing the position checked
+
+'''
+
+'''
+
+    39 51 7 14 3 86
+
+  39 51 7      14 3 68
+ 39 51  7    14 3   68
+ 39  51  7    14  3  68
+ 39 51  7     3 14  68
+7  39  51     3  14  68
+3   7   14   39   51   68
+
+
 '''
