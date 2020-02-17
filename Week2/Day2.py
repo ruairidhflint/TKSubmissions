a18f796429b355e81eee4a1169d0a9cdaa4612cf
Rory Flint <roryflintphoto@gmail.com>
2020-02-17 22:03:51 +0000

    Add second recursive search functionality

--- Week2/Day2.py
+++ Week2/Day2.py
@@ -12,3 +12,20 @@ def recursive_search(arr, target):
 
 
 print(recursive_search([1,2,3,4,5,6,7,8], 5))
+
+def recursive_search_two(arr,target,position=0):
+    if position == len(arr) - 1:
+        return False
+
+    if arr[position] == target:
+        return True
+    else: 
+        return recursive_search_two(arr, target, position + 1)
+
+print(recursive_search_two([1,2,3,4,5,6,7,8], 50))
+
+'''What would be the base case(s) we’d have to consider for implementing this function?
+
+- For first implementation: If the list has length or if the list target and the list position match
+- For second implementation: if the position is equal to the length of the list
+'''
