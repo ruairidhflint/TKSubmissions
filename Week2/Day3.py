a6cc5dfff64cbad4d2ab1bbc8028d58cfcc0010a
Rory Flint <roryflintphoto@gmail.com>
2020-02-18 22:45:52 +0000

    Start Day 3 Week 2

---
+++ Week2/Day3.py
@@ -0,0 +1,22 @@
+'''Given an image represented by an NxN matrix, where each pixel in the image is an integer from 0 to 9, write a function rotate_image that receives a matrix as input and rotates the image by 90 degrees in the counter-clockwise direction.
+
+Thoughts: 
+
+[[1, 2, 3, 4],
+ [5, 6, 7, 8],
+ [9, 1, 2, 3],
+ [4, 5, 6, 7]]
+
+Rotated 90 degrees clockwise
+
+[[4, 9, 5, 1],
+ [5, 1, 6, 2],
+ [8, 2, 7, 3],
+ [7, 3, 8, 4]]
+
+ After a 90 degree rotatation, the first elements in each array become the first array, the second elements
+ become the ordered elements in the second array etc etc. 
+ 
+
+
+'''
