1e58c4c2597c6772883588e8b649e5e04677e8db
Rory Flint <roryflintphoto@gmail.com>
2020-02-19 07:35:41 +0000

    Complexity questions answered

--- Week2/Day3.py
+++ Week2/Day3.py
@@ -26,6 +26,8 @@ test_case = [[1, 2, 3, 4],
              [9, 1, 2, 3],
              [4, 5, 6, 7]]
 
+test_case2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
 
 def rotate_image(matrix):
     # create empty matrix container (list of four empty lists)
@@ -45,9 +47,42 @@ def rotate_image(matrix):
 
     return rotated_image
 
+
 '''
 Run time analysis...very poor. O(n^2) (nested loops).
 
 '''
 
-print(rotate_image(test_case))
+print(rotate_image(test_case2))
+
+
+# Classify the runtimes of each of the following functions:
+import math 
+
+def foo(n):
+    sq_root = int(math.sqrt(n)) # O(1) ?
+    for i in range(0, sq_root): # O(n)
+        print(i) # O(1)
+
+# Answer = O(n)
+
+
+def bar(x):
+  sum = 0
+  for i in range(0, 1463): #O(n)
+    i += sum
+    for j in range(0, x): #O(n)
+      for k in range(x, x + 15): #O(n)
+        sum += 1
+
+# Answer = O(n^3)
+
+def baz(array):
+  print(array[1]) # O(1)
+  midpoint = len(array) // 2 # O(1)
+  for i in range(0, midpoint): # O(n/2)
+    print(array[i]) #O(1)
+  for _ in range(1000): #O(1000)
+    print('hi') #O(1)
+
+# Answer = O(1 + 1 + n/2 + 1000 + 1) = O(n)
